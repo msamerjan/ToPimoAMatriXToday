3a263c90009783a9566f32e7e56000bee0f168d0
msamerjan <msamerjan@lclark.edu>
2016-04-16 11:51:19 -0700

    blah blah

--- app/src/main/java/com/mobiledev/topimpamatricks/Keyboard/MathKeyboardView.java
+++ app/src/main/java/com/mobiledev/topimpamatricks/Keyboard/MathKeyboardView.java
@@ -1,11 +1,17 @@
 package com.mobiledev.topimpamatricks.Keyboard;
 
+import android.content.Context;
 import android.view.View;
 
 /**
  * Created by maiaphoebedylansamerjan on 4/13/16.
  */
 public class MathKeyboardView extends View implements View.OnClickListener{
+
+    public MathKeyboardView(Context context) {
+        super(context);
+    }
+
     @Override
     public void onClick(View v) {
 
